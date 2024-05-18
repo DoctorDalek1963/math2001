0a23f3a13e7658fad5ba07fa42ca20e38b84a117
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-18 13:32:05 +0100

    Complete section 2.2

--- Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
+++ Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
@@ -12,21 +12,49 @@ example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
     _ < 1 := by numbers
 
 example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
-  sorry
+  apply ne_of_gt
+  calc
+    y ^ 2 + 1
+    _ ≥ 1 := by extra
+    _ > 0 := by numbers
 
 example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
   apply le_antisymm
-  calc
-    a ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
-    _ = 0 := h1
-  extra
 
+  -- a ^ 2 ≤ 0
+  calc
+    a ^ 2
+    _ ≤ a ^ 2 + b ^ 2 := by extra
+    _ = 0 := h1
+
+  -- a ^ 2 ≥ 0
+  extra
 
 /-! # Exercises -/
 
-
 example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by
-  sorry
+  apply ne_of_gt
+  have h : m = 4 := by addarith [hm]
+  calc
+    3 * m
+    _ = 3 * 4 := by rw [h]
+    _ > 6 := by numbers
 
 example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
-  sorry
+  apply le_antisymm
+
+  -- s ≤ -2
+  have h : 3 * s ≤ 3 * -2 := by calc
+    3 * s
+    _ ≤ -6 := h1
+    _ = 3 * -2 := by numbers
+
+  cancel 3 at h
+
+  -- s ≥ -2
+  have h : 2 * s ≥ 2 * -2 := by calc
+    2 * s
+    _ ≥ -4 := h2
+    _ = 2 * -2 := by numbers
+
+  cancel 2 at h
